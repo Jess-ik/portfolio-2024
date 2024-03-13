7ee81d9bdfc73dd5c4946d28173dfa8cad58f6a8
Jess <jessica.louvel@gmail.com>
2024-03-13 14:52:04 +0100

    add 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className='bg-dark h-screen w-screen flex flex-col justify-center items-center gap-8'>
       <Logo404 />
       <p className='text-light'>Ooops! Looks like you got lost...</p>
-      <Link href="/" className='text-light rounded-full px-4 py-2 border hover:bg-light hover:text-dark transition ease-in-out !duration-500'>Let's go home</Link>
+      <Link href="/" className='text-light rounded-full px-4 py-2 border hover:bg-light hover:text-dark transition ease-in-out !duration-500'>Let&apos;s go home</Link>
     </div>
   )
 }
